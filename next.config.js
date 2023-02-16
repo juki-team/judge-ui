31a7ee9053a21327c874e728c661333c9afcfe51
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-02-16 15:17:06 -0400

    Fixing public icons

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withPWA = require('next-pwa')({
-	dest: 'public',
-	// disable: process.env.NODE_ENV === 'development',
-	// disable: true,
-	// register: true,
-	// scope: '/app',
-	// sw: 'service-worker.js',
-	//...
+    dest: 'public'
+    // disable: process.env.NODE_ENV === 'development',
+    // disable: true,
+    // register: true,
+    // scope: '/app',
+    // sw: 'service-worker.js',
+    //...
 });
 
 // const withTM = require('next-transpile-modules')(['react-markdown']);
@@ -18,37 +18,37 @@ const withPWA = require('next-pwa')({
 
 // module.exports = withTM(withPWA({
 module.exports = withPWA({
-	i18n: {
-		locales: ['en', 'es'],
-		defaultLocale: 'en',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'images.juki.pub',
-				pathname: '/u/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.juki.pub',
-				pathname: '/c/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'juki-judge.s3.us-east-2.amazonaws.com',
-				pathname: '/public/user/image/**',
-			},
-		],
-	},
-	async redirects() {
-		return [
-			{
-				source: '/admin',
-				destination: '/admin/users',
-				permanent: true,
-			},
-		];
-	},
-	// future: { webpack5: true },
+    i18n: {
+        locales: ['en', 'es'],
+        defaultLocale: 'en'
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.juki.pub',
+                pathname: '/u/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.juki.pub',
+                pathname: '/c/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'juki-judge.s3.us-east-2.amazonaws.com',
+                pathname: '/public/user/image/**'
+            }
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/admin',
+                destination: '/admin/users',
+                permanent: true
+            }
+        ];
+    }
+    // future: { webpack5: true },
 });
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,12 +12,12 @@ export default function Document() {
         <meta name="description" content="Juki Judge is designed to make people improve their programming skills" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
         <meta name="msapplication-TileColor" content="#164066" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#FFFFFF" />
-    
+  
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
@@ -29,14 +29,14 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
         {/*<link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />*/}
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
-    
+  
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#164066" />
         <link rel="shortcut icon" href="/icons/favicon.ico" />
         {/*<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />*/}
-    
+  
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="https://judge.juki.app" />
         <meta name="twitter:title" content="Juki Judge App" />
