eb67433538077a6f970f75e3a896ae97eaa7b8ff
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2023-04-20 02:21:47 -0400

    Fixing CF language selector

--- pages/api/version.ts
+++ pages/api/version.ts
@@ -1,5 +1,5 @@
 import { contentResponse } from 'helpers';
 
 export default function handler(req, res) {
-  res.status(200).json(contentResponse('ok', { version: '1.1.0' }));
+  res.status(200).json(contentResponse('ok', { version: '1.1.1' }));
 }
