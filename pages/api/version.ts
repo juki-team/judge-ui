e7616d5a8670c95b90df4fa6caff3d2070695828
OscarGauss <me@oscargauss.com>
2023-05-15 06:50:25 -0400

    v1.2.0

--- pages/api/version.ts
+++ pages/api/version.ts
@@ -1,5 +1,5 @@
 import { contentResponse } from 'helpers';
 
 export default function handler(req, res) {
-  res.status(200).json(contentResponse('ok', { version: '1.1.2' }));
+  res.status(200).json(contentResponse('ok', { version: '1.2.0' }));
 }
