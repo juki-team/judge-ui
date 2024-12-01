8f8078b4c71f0952dd53aef25e4a423b9548b422
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2024-12-01 16:45:25 -0400

    upd libs

--- next.config.js
+++ next.config.js
@@ -28,17 +28,6 @@ module.exports = withPlugins([
     
     return config;
   },
-  experimental: {
-    // esmExternals: 'loose',
-    //   transpilePackages: [
-    //     '@react-dnd/asap',
-    //     '@react-dnd/invariant',
-    //     '@react-dnd/shallowequal',
-    //     'dnd-core',
-    //     'react-dnd',
-    //     'react-dnd-html5-backend',
-    //   ],
-  },
   publicRuntimeConfig: {
     version,
   },
