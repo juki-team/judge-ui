b89c58f294dbab658ddf48cd43307f15c81aa753
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-03-22 11:11:32 -0400

    fixing ide page

--- src/app/(session)/ide/page.tsx
+++ src/app/(session)/ide/page.tsx
@@ -12,6 +12,7 @@ export default function IDEPage() {
           label: PROGRAMMING_LANGUAGE[language].label,
           value: language,
         }))}
+        enableAddCustomSampleCases
       />
     </div>
   );
