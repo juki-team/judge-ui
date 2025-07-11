f7d98a22e45c18e8f30e25a35142135a5b384b59
Oscar Gauss Carvajal Yucra <me@oscargauss.com>
2025-07-11 09:36:13 -0400

    upd libs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,7 @@
-import {
-  jukiApiSocketManager as _jukiApiSocketManager,
-  jukiAppRoutes as _jukiAppRoutes,
-} from '@juki-team/base-ui/settings';
+import { jukiApiSocketManager, jukiAppRoutes } from '@juki-team/base-ui/settings';
 import { JUKI_SERVICE_V1_URL, JUKI_SERVICE_V2_URL, JUKI_SOCKET_BASE_URL, JUKI_TOKEN_NAME } from 'src/constants';
-
-export const jukiAppRoutes = _jukiAppRoutes;
-export const jukiApiSocketManager = _jukiApiSocketManager;
 
 jukiApiSocketManager.setApiSettings(JUKI_SERVICE_V1_URL, JUKI_SERVICE_V2_URL, JUKI_TOKEN_NAME);
 jukiApiSocketManager.setSocketSettings(JUKI_SOCKET_BASE_URL);
+
+export { jukiApiSocketManager, jukiAppRoutes };
