f525705b8bcef4edb1bad8f75dec74fc3830c625
Oscar Gauss Carvajal Yucra <dev@oscargauss.com>
2025-11-16 17:13:44 -0400

    fix build

--- src/components/ScreenshotFrames.tsx
+++ src/components/ScreenshotFrames.tsx
@@ -1,16 +1,14 @@
-import domToImage from 'dom-to-image-more';
-
-const toPng = async () => {
-  const cmThemeNode = document.querySelector('#juki-app');
-  if (!cmThemeNode) {
-    return;
-  }
-  try {
-    return await domToImage.toBlob(cmThemeNode);
-  } catch (error) {
-    console.error('Error al capturar imagen:', error);
-  }
-};
-
+// const toPng = async () => {
+//   const cmThemeNode = document.querySelector('#juki-app');
+//   if (!cmThemeNode) {
+//     return;
+//   }
+//   try {
+//     return await domToImage.toBlob(cmThemeNode);
+//   } catch (error) {
+//     console.error('Error al capturar imagen:', error);
+//   }
+// };
+//
 export const ScreenshotFrames = () => {
 };
