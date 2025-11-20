edc3ce7c90f447496886989a5205533fe9cb20d0
Oscar Gauss Carvajal Yucra <dev@oscargauss.com>
2025-11-20 11:01:04 -0400

    udd libs

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -101,43 +101,41 @@ export const NavigationBar = ({ children }: PropsWithChildren<{}>) => {
   );
   
   return (
-    <>
-      <MainMenu
-        onSeeMyProfile={() => pushRoute(jukiAppRoutes.JUDGE().profiles.view({ nickname: userNickname }))}
-        menu={menu}
-        profileSelected={pathname.includes('/profiles/')}
-        onBack={pathname !== backPah ? () => {
-          pushRoute(backPah);
-        } : undefined}
-        moreApps={companyKey == JUKI_APP_COMPANY_KEY && <>
-          <Link href="https://utils.juki.app">
-            <div className="jk-row gap left">
-              <Image
-                width={100}
-                height={50}
-                src={`https://images.juki.pub/assets/juki-utils-horizontal-${userPreferredTheme === Theme.DARK ? 'white' : 'color'}-logo.png`}
-                alt="juki utils"
-              />
-              <div className="link">utils.juki.app</div>
+    <MainMenu
+      onSeeMyProfile={() => pushRoute(jukiAppRoutes.JUDGE().profiles.view({ nickname: userNickname }))}
+      menu={menu}
+      profileSelected={pathname.includes('/profiles/')}
+      onBack={pathname !== backPah ? () => {
+        pushRoute(backPah);
+      } : undefined}
+      moreApps={companyKey == JUKI_APP_COMPANY_KEY && <>
+        <Link href="https://utils.juki.app">
+          <div className="jk-row gap left">
+            <Image
+              width={100}
+              height={50}
+              src={`https://images.juki.pub/assets/juki-utils-horizontal-${userPreferredTheme === Theme.DARK ? 'white' : 'color'}-logo.png`}
+              alt="juki utils"
+            />
+            <div className="link">utils.juki.app</div>
+          </div>
+        </Link>
+        <Link href="https://coach.juki.app">
+          <div className="jk-row gap left">
+            <Image
+              width={100}
+              height={50}
+              src={`https://images.juki.pub/assets/juki-coach-horizontal-${userPreferredTheme === Theme.DARK ? 'white' : 'color'}-logo.png`}
+              alt="juki coach"
+            />
+            <div className="jk-row nowrap" style={{ alignItems: 'baseline' }}>
+              <T className="link">coach.juki.app</T>
             </div>
-          </Link>
-          <Link href="https://coach.juki.app">
-            <div className="jk-row gap left">
-              <Image
-                width={100}
-                height={50}
-                src={`https://images.juki.pub/assets/juki-coach-horizontal-${userPreferredTheme === Theme.DARK ? 'white' : 'color'}-logo.png`}
-                alt="juki coach"
-              />
-              <div className="jk-row nowrap" style={{ alignItems: 'baseline' }}>
-                <T className="link">coach.juki.app</T>
-              </div>
-            </div>
-          </Link>
-        </>}
-      >
-        {children}
-      </MainMenu>
-    </>
+          </div>
+        </Link>
+      </>}
+    >
+      {children}
+    </MainMenu>
   );
 };
